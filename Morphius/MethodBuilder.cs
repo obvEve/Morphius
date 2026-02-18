c2b51acd3321e6942dce6d2e55c68cba41f940f0
Misfiy <85962933+obvEve@users.noreply.github.com>
2026-02-18 20:49:33 +0100

    Replace MethodCache with MethodBuilder

---
+++ Morphius/MethodBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace Morphius;
+
+/// <summary>
+/// Handles creating a new method based on the original, and its replacements using SL
+/// </summary>
+internal class MethodBuilder
+{
+    private readonly MethodInfo _original;
+    
+    internal MethodBuilder()
+    {
+    }
+
+    private void Build()
+    {
+        // Build the new method
+    }
+}
